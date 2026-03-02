f3c897b68d1fdf5dace7c8c322470e62c1e46400
binh4k <binh4k@users.noreply.github.com>
2026-03-02 10:48:58 +0700

    fix: add missing daily_watch station to easy week 4 index.js
    Former-commit-id: 0a940a996ff633bee5d2616b31360ea74cd72ffc
    Former-commit-id: 705bd79b5124b9b36a702a8bc6a667ee026d6bb2

--- src/data/weeks_easy/week_04/index.js
+++ src/data/weeks_easy/week_04/index.js
@@ -10,6 +10,7 @@ import explore from './explore.js';
 import word_power from './word_power.js';
 import mindmap from './mindmap.js';
 import word_match from './word_match.js';
+import daily_watch from './daily_watch.js';
 
 const weekData = {
   weekId: 4,
@@ -39,7 +40,8 @@ const weekData = {
     shadowing: shadowing,
     writing: writing,
     explore: explore,
-    mindmap_speaking: mindmap
+    mindmap_speaking: mindmap,
+    daily_watch: daily_watch
   }
 };
 
