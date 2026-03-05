5b2bb815410f80d66fd1d631e9d631cb5b317993
binh4k <binh4k@users.noreply.github.com>
2026-03-05 13:00:19 +0700

    Fix Daily Watch videos: Add Week 9-10 to Blueprint + Optimize video queries for better purpose/theme matching
    Former-commit-id: ad8d7645f0693be9640088cf1aca2f8df8847b03
    Former-commit-id: b08aef2eca905ad56c0608e7775b4b076836399c

--- src/data/weeks_easy/week_09/daily_watch.js
+++ src/data/weeks_easy/week_09/daily_watch.js
@@ -1,10 +1,10 @@
 export default {
   videos: [
     { id: 1, title: "Big, Big, Big | Adjectives Song for Kids", videoId: "3JZi2oDvPs4", duration: "01:28", sim_duration: 88, thumb: "https://img.youtube.com/vi/3JZi2oDvPs4/mqdefault.jpg" },
-    { id: 2, title: "Where Are You Going? | Places Song | Kids Learning Song | ESL for Kids | Fun Kids English", videoId: "FxRGkjkVTGA", duration: "02:40", sim_duration: 160, thumb: "https://img.youtube.com/vi/FxRGkjkVTGA/mqdefault.jpg" },
-    { id: 3, title: "English for Kids - Super Simple Songs", videoId: "e54m6XOpRgU", duration: "01:58", sim_duration: 118, thumb: "https://img.youtube.com/vi/e54m6XOpRgU/mqdefault.jpg" },
-    { id: 4, title: "Let&#39;s Play! | Learning Songs | Conversation 1 | Little Fox | Animated Songs for Kids", videoId: "UYbO_wBOW90", duration: "01:04", sim_duration: 64, thumb: "https://img.youtube.com/vi/UYbO_wBOW90/mqdefault.jpg" },
-    { id: 5, title: "Science for Kids - Learning Video", videoId: "V4Ij8hE5TsI", duration: "05:00", sim_duration: 300, thumb: "https://img.youtube.com/vi/V4Ij8hE5TsI/mqdefault.jpg" }
+    { id: 2, title: "Theme &quot;City&quot;  (Town, Job) - Kids Vocabulary - Words Theme collection - Educational video", videoId: "x39KwsXxl_0", duration: "09:16", sim_duration: 556, thumb: "https://img.youtube.com/vi/x39KwsXxl_0/mqdefault.jpg" },
+    { id: 3, title: "Apple Picking | Bat and Friends | Little Fox Stories Level 1 | Stories for Kindergarten", videoId: "6qIx7MoAclg", duration: "14:50", sim_duration: 890, thumb: "https://img.youtube.com/vi/6qIx7MoAclg/mqdefault.jpg" },
+    { id: 4, title: "10 Little Airplanes | Kids Songs | Count To Ten | Super Simple Songs", videoId: "VNRNAloCb5Y", duration: "02:07", sim_duration: 127, thumb: "https://img.youtube.com/vi/VNRNAloCb5Y/mqdefault.jpg" },
+    { id: 5, title: "Community Helpers for Kids | Jobs & Occupations for Preschool and Kindergarten | Kids Academy", videoId: "jt2q1cHsH6E", duration: "04:18", sim_duration: 258, thumb: "https://img.youtube.com/vi/jt2q1cHsH6E/mqdefault.jpg" }
   ],
   bonus_games: [{title: "Game", url: "#", description: "Review"}]
 };
