a25b42d4268b5f5ce88ce54b23ee4d092e24e8ca
binh4k <binh4k@users.noreply.github.com>
2026-03-09 12:22:28 +0700

    Week 12 UI fixes: remove sentence_builder, add explore image_url, fix Easy daily_watch import
    Former-commit-id: ad8fe558c5cc2182ee0b54dab3a50aacec19cb2e
    Former-commit-id: 72166af8f57d23e4aa4a68766ef3bb2bd404350e

--- src/data/weeks_easy/week_12/explore.js
+++ src/data/weeks_easy/week_12/explore.js
@@ -1,6 +1,7 @@
 export default {
   title_en: "Hobbies I Love",
   title_vi: "Sở thích tôi yêu",
+  image_url: "/images/week12_easy/explore_cover_w12.jpg",
   content_en: `I have many hobbies! I can **sing** my favorite songs at home. I can **dance** when I listen to music. On weekends, I **ride** my bike in the park. Sometimes I **draw** pictures of my family and pets. I like to **swim** in the summer. I also **cook** simple food with my mom. I cannot do everything perfectly, but I love trying new things!`,
   content_vi: `Tôi có nhiều sở thích! Tôi có thể hát những bài hát yêu thích ở nhà. Tôi có thể nhảy khi nghe nhạc. Cuối tuần, tôi đạp xe trong công viên. Đôi khi tôi vẽ tranh gia đình và thú cưng. Tôi thích bơi vào mùa hè. Tôi cũng nấu món ăn đơn giản với mẹ. Tôi không thể làm mọi thứ hoàn hảo, nhưng tôi thích thử những điều mới!`,
   audio_narration: "/audio/week12_easy/explore_narration.mp3",
