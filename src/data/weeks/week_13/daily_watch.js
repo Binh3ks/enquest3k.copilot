230ef91ef351651d3e63c0647389f9f7bee4bc42
binh4k <binh4k@users.noreply.github.com>
2026-03-10 14:02:14 +0700

    feat(week13): Add Week 13 - Daily Routines complete content
    - AI Tutor: 3 missions (My Morning Routine, My Perfect Day, The Magic Clock)
    - Advanced: 14 stations with Present Simple grammar, daily routine vocab
    - Easy: 14 independent stations with Tier 1 vocabulary
    - Audio: 239 files generated (139 Advanced + 100 Easy)
    - Videos: 5 YouTube videos curated
    - UI: Updated StoryMissionTab, FreeTalkTab, gameAdaptation
    - Config: Added Week 13 to CDN_WEEKS array
    
    Theme: Daily Routines | Grammar: Present Simple | Math: Telling Time
    
    Former-commit-id: 6016d1030a08c87a971dc1e4d8bb0f250624a576
    Former-commit-id: bb17d766c0a854d7146b82cd7848dddf4700fa84

---
+++ src/data/weeks/week_13/daily_watch.js
@@ -0,0 +1,10 @@
+export default {
+  videos: [
+    { id: 1, title: "The Present Simple Song. English with Us.", videoId: "JiAKwjwx5H0", duration: "02:11", sim_duration: 131, thumb: "https://img.youtube.com/vi/JiAKwjwx5H0/mqdefault.jpg" },
+    { id: 2, title: "Wake Up! Daily Routines Song for Kids", videoId: "eUXkj6j6Ezw", duration: "02:52", sim_duration: 172, thumb: "https://img.youtube.com/vi/eUXkj6j6Ezw/mqdefault.jpg" },
+    { id: 3, title: "Teddy&#39;s Day | Early Learning | Phonics | Little Fox | Bedtime Stories", videoId: "MvbnocOuOTw", duration: "01:06", sim_duration: 66, thumb: "https://img.youtube.com/vi/MvbnocOuOTw/mqdefault.jpg" },
+    { id: 4, title: "Good Morning, Good Night | Learning Songs | Little Fox | Animated Songs for Kids", videoId: "mebfKDQ4dLo", duration: "02:02", sim_duration: 122, thumb: "https://img.youtube.com/vi/mebfKDQ4dLo/mqdefault.jpg" },
+    { id: 5, title: "Science for Kids - Learning Video", videoId: "V4Ij8hE5TsI", duration: "05:00", sim_duration: 300, thumb: "https://img.youtube.com/vi/V4Ij8hE5TsI/mqdefault.jpg" }
+  ],
+  bonus_games: [{title: "Game", url: "#", description: "Review"}]
+};
