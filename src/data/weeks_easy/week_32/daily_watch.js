4c2c308dcadd1a4868e3df14476ceecead9adbce
binh4k <binh4k@users.noreply.github.com>
2026-05-16 09:26:18 +0700

    fix(W32): update Daily Watch videos + Story Remix robustness + Singapore Math bar model fallback
    Former-commit-id: e9f3f66602ec03b12c8dbc8f5bfe8b98c68239a1
    Former-commit-id: ee9592190703c6142ab43f19343afee8c83f00b6

--- src/data/weeks_easy/week_32/daily_watch.js
+++ src/data/weeks_easy/week_32/daily_watch.js
@@ -1,10 +1,10 @@
 export default {
   videos: [
-    { id: 1, title: "Basic English Grammar | Be verbs AM IS ARE", videoId: "5kjCcBsBzUs", duration: "03:07", sim_duration: 187, thumb: "https://img.youtube.com/vi/5kjCcBsBzUs/mqdefault.jpg" },
+    { id: 1, title: "10 Irregular Verbs for Kids", videoId: "1MCeY73Qdc4", duration: "01:57", sim_duration: 117, thumb: "https://img.youtube.com/vi/1MCeY73Qdc4/mqdefault.jpg" },
     { id: 2, title: "Irregular Past Tense Verbs Grammar Practice", videoId: "oJZdkhclusQ", duration: "08:17", sim_duration: 497, thumb: "https://img.youtube.com/vi/oJZdkhclusQ/mqdefault.jpg" },
     { id: 3, title: "Peter Rabbit & Benjamin Bunny: Laundry Day Chaos! | Little Fox", videoId: "iRk9TyCN8HY", duration: "10:32", sim_duration: 632, thumb: "https://img.youtube.com/vi/iRk9TyCN8HY/mqdefault.jpg" },
     { id: 4, title: "We Went Camping - Irregular Verbs", videoId: "ofu_89J_HVI", duration: "07:05", sim_duration: 425, thumb: "https://img.youtube.com/vi/ofu_89J_HVI/mqdefault.jpg" },
-    { id: 5, title: "Science for Kids - Learning Video", videoId: "V4Ij8hE5TsI", duration: "05:00", sim_duration: 300, thumb: "https://img.youtube.com/vi/V4Ij8hE5TsI/mqdefault.jpg" }
+    { id: 5, title: "Keeping Our Water Clean! | SciShow Kids", videoId: "Npv47F3GK2w", duration: "04:54", sim_duration: 294, thumb: "https://img.youtube.com/vi/Npv47F3GK2w/mqdefault.jpg" }
   ],
   bonus_games: [{title: "Game", url: "#", description: "Review"}]
 };
