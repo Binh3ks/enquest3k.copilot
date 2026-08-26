ef7d160cfbc3d5987e5c9b7393e03b8b0a461c9a
binh4k <binh4k@users.noreply.github.com>
2026-08-26 09:31:00 +0700

    fix(S2/S3): align component names with actual files

---
+++ src/components/cambridge/NotepadNoteCompleter.jsx
@@ -0,0 +1,2 @@
+export { default, NotepadNoteCompleter } from '../common/NotepadNoteCompleter';
+export { default } from '../common/NotepadNoteCompleter';
---
+++ src/components/cambridge/StoryWriting.jsx
@@ -0,0 +1,2 @@
+export { default, StoryWriting } from '../../modules/write_speak/StoryWriting';
+export { default } from '../../modules/write_speak/StoryWriting';
